f33c5d043994497b07ccd28191e9057e88a63a4a
Daniel Ireson <danielireson@gmail.com>
2021-05-20 20:56:16 +0100

    feat: add static view components

--- src/main/localkanban/app/core.cljs
+++ src/main/localkanban/app/core.cljs
@@ -3,8 +3,70 @@
 
 ;;; Views
 
+(defn navbar-component []
+  [:nav.navbar {:role "navigation" :aria-label "navigation"}
+   [:div.navbar-brand
+    [:a.navbar-item {:href "/"} [:span "LocalKanban"]]]
+   [:div.navbar-end
+    [:div.navbar-item
+     [:div.buttons
+      [:a.button.is-primary "Add another list"]]]]])
+
+(defn list-modal-component []
+  [:div.modal
+   [:div.modal-background]
+   [:div.modal-card
+    [:header.modal-card-head
+     [:p.modal-card-title "Add another list"]
+     [:button.delete {:aria-label "close"}]]
+    [:section.modal-card-body
+     [:p
+      [:input.input {:type "text" :placeholder "Enter list name"}]]]
+    [:footer.modal-card-foot
+     [:button.button.is-primary "Done"]]]])
+
+(defn card-modal-component []
+  [:div.modal
+   [:div.modal-background]
+   [:div.modal-card
+    [:header.modal-card-head
+     [:p.modal-card-title "Add card"]
+     [:button.delete {:aria-label "close"}]]
+    [:section.modal-card-body
+     [:p
+      [:textarea.textarea {:placeholder "Enter card description"}]]]
+    [:footer.modal-card-foot
+     [:button.button.is-primary "Done"]]]])
+
+(defn card-component []
+  [:div.card
+   [:div.card-content
+    [:div.content "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus."]]])
+
+(defn cards-component []
+  [:div.cards
+   [card-component]
+   [card-component]
+   [card-component]
+   [card-component]])
+
+(defn list-component []
+  [:div.list
+   [:h1.list-title "List title"]
+   [cards-component]
+   [:div.list-footer
+    [:a "Add card"]]])
+
+(defn lists-component []
+  [:div.columns.is-mobile.is-vcentered
+   [:div.column [list-component]]])
+
 (defn app []
-  [:h1 "Create Reagent App"])
+  [:div.application
+   [navbar-component]
+   [lists-component]
+   [list-modal-component]
+   [card-modal-component]])
 
 ;;; Render
 
