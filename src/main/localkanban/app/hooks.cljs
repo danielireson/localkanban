388884a7c5a5c1c0eb796d83ae2faf3275124866
Daniel Ireson <danielireson@gmail.com>
2021-06-24 23:35:23 +0100

    fix: ensure cursor is last for autofocus

--- src/main/localkanban/app/hooks.cljs
+++ src/main/localkanban/app/hooks.cljs
@@ -2,7 +2,12 @@
   (:require [react :as react]))
 
 (defn- autofocus-modal []
-  (.focus (.querySelector js/document ".modal.is-active input, .modal.is-active textarea")))
+  (let [element (.querySelector js/document ".modal.is-active input, .modal.is-active textarea")
+        value (.-value element)]
+    (.focus element)
+    ;; push cursor to end by reseting value
+    (set! (.-value element) "")
+    (set! (.-value element) value)))
 
 (defn use-autofocus-modal [should-autofocus]
   (react/useEffect #(when should-autofocus (autofocus-modal)) (clj->js [should-autofocus])))
